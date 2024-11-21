a3fb995eda1579dea6744d78df5a9ac2f3439f31
k0kx1 <tastykifi10@gmail.com>
2024-11-21 13:49:03 +0100

    Update zadanie10.py

---
+++ zadanie10.py
@@ -0,0 +1,7 @@
+#program tworzy liste liczb od 1 do 10 i sprawdza czy aktualna liczba jest równa 5
+liczby = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for liczba in liczby:
+    if liczba == 5:
+        break
+    print(liczba)
