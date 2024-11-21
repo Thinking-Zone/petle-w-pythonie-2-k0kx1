bc9224da2b45c9d1f843ed59e0a7c15f5e0eddba
k0kx1 <tastykifi10@gmail.com>
2024-11-21 13:37:36 +0100

    Update zadanie7.py

--- zadanie7.py
+++ zadanie7.py
@@ -1,19 +1,10 @@
-pada = False
-licznik_nie = 0
+import random
 
 
-while not pada:
-    odpowiedz = input("czy pada? (tak/nie/nie wiem): ").lower()
-    
-    if odpowiedz == "tak":
-        pada = True
-    elif odpowiedz == "nie":
-        licznik_nie += 1
-    elif odpowiedz == "nie wiem":
-        print("To sobie pójdzi na zewnątrz i se zoba.")
-    else:
-        print("nie kumam. Proszę odpowiedz 'tak', 'nie' lub 'nie wiem'.")
-        
-        
-    print(f"liczba odpowiedzi 'nie': {licznik_nie}")    
+pogoda = random.choice(['pada', 'nie pada'])
 
+
+while input("czy pada deszcz? (pada/nie pada): ") != pogoda:
+    print("nie zgadłeś, spróbuj jeszcze ra.")
+
+print("brawo! zgadłeś!")
